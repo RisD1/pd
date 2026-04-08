d120b11549241d1fa863ac8b76769b9b70f7b7f8
Rishika <rishika0707@gmail.com>
2026-04-09 03:03:06 +0530

    fix port

--- app.py
+++ app.py
@@ -271,5 +271,8 @@ def handle_disconnect():
     send_games_update()
 
 
+import os
+
 if __name__ == '__main__':
-    socketio.run(app)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
